98b7d3ae53e5d50fe1a6fd58deff804a35829855
ahnseonghyun <sh84.ahn@gmail.com>
2020-03-18 17:18:31 +0900

    Add config

---
+++ config/config.go
@@ -0,0 +1,29 @@
+package config
+
+import (
+	"os"
+)
+
+type Config struct {
+	NutanixUrl string
+	UserName   string
+	Password   string
+}
+
+func MakeConfig() Config {
+	NutanixUrl := os.Getenv("NUTANIX_URL")
+	UserName := os.Getenv("NUTANIX_USERNAME")
+	Password := os.Getenv("NUTANIX_PASSWORD")
+	if len(NutanixUrl) == 0 {
+		panic("no value NUTANIX_URL")
+	}
+	if len(UserName) == 0 {
+		panic("no value NUTANIX_USERNAME")
+	}
+
+	if len(Password) == 0 {
+		panic("no value NUTANIX_PASSWORD")
+	}
+
+	return Config{NutanixUrl: NutanixUrl, UserName: UserName, Password: Password}
+}
